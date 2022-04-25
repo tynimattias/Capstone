b998165367991794275c8312dead02d49c2edf42
tynimattias <72426180+tynimattias@users.noreply.github.com>
2022-04-24 19:42:11 -0700

    Updated Ultrasonic class

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -9,16 +9,14 @@ import RPi.GPIO as gpio
 class Ultrasonic_Sensor:
     
     
-    edge_count = 0
-    time_measure0 = 0
-    time_measure1 = 0
-    timer_value1 = 0
-    timer_value2  = 0
+    
     
     def __init__(self, pin, trigger):
         self.pin = pin
         self.trigger = trigger
-        
+        self.edge_count = 0
+        self.timer_value1 = 0
+        self.timer_value2  = 0
     
     def setup(self):
         print(f"setup ultrasonic sensor on pin {self.pin}")
@@ -31,33 +29,37 @@ class Ultrasonic_Sensor:
         print(f"Trigger setup on pin {self.trigger}")
     
     def measure(self):
-        gpio.add_event_detect(self.pin, gpio.BOTH)
-        gpio.add_event_callback(self.pin, Ultrasonic_Sensor.interrupt_handler)
-        Ultrasonic_Sensor.pulse(self)
+        
+        average_array = np.zeros(5)
+        
+        for i in range(len(average_array)):
+            Ultrasonic_Sensor.pulse(self)
+            while(gpio.input(self.pin)==0):
+                self.timer_value2 = time.time()
+            while(gpio.input(self.pin)==1):
+                self.timer_value1 = time.time()  
+            self.timer_value_1 = time.time()
+            
+            pulse = (self.timer_value1 - self.timer_value2)
+            
+            sleep(0.03)
+            distance = (0.5)*(pulse)*343
+            
+            average_array[i] = distance
+            
         
         
-        pulse = (Ultrasonic_Sensor.timer_value2 - Ultrasonic_Sensor.timer_value1)
         
-        print(f"Pulse is {pulse}")
-        distance = (0.5)*(pulse)*343
-        gpio.remove_event_detect(self.pin)
-        return distance
+        return sum(average_array)/len(average_array) *100
         
     
     def pulse(self):
         
         gpio.output(self.trigger, gpio.HIGH)
-        sleep(10E-6)
+        sleep(15E-6)
         gpio.output(self.trigger, gpio.LOW)
         
-    def interrupt_handler(self):
-        
-        if(Ultrasonic_Sensor.edge_count == 0):
-            Ultrasonic_Sensor.timer_value1 = time.time()*1000
-            edge_count +=1
-        if(Ultrasonic_Sensor.edge_count == 1):
-            Ultrasonic_Sensor.timer_value2 = time.time()*1000
-            edge_count =0
+    
     
     
     def __del__(self):
